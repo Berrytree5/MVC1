aeacd13a6380854a511f1d2f923d6ced6d1051bb
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-28 13:38:37 -0700

    updated packages

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 class User extends Model {
   // Method to check if a password matches the hashed password in the database
