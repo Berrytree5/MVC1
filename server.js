89a023f2f774dbf13fac726a3016532206f61a3e
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-29 12:00:29 -0700

    cleaned up code

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
+// Imports
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
-const path = require("path");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const dotenv = require("dotenv");
-
-dotenv.config();
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
-const routes = require("./controllers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Define session cookie properties
+const sessionConfig = {
+  secret: "longrandompass3431", 
+  cookie: {
+    maxAge: 1200000, // Session expiration time (in milliseconds)
+    httpOnly: true, // Ensure cookies are only accessible via HTTP(S)
+    secure: false, // Set to true when using HTTPS
+    sameSite: "strict", // Protect against cross-site request forgery (CSRF) attacks
+  },
+  resave: false, // Don't save sessions if unmodified
+  saveUninitialized: true, // Save new sessions
+  store: new SequelizeStore({
+    db: sequelize, // Use Sequelize for session storage
+  }),
+};
+
+app.use(session(sessionConfig));
+
+// Set the view engine to Handlebars
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// Specify the location of your HTML templates
-app.set("views", path.join(__dirname, "views"));
-
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded form data
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the "public" directory
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static('utils'));
+app.use(routes); // Use the defined routes
 
-// Configure and use sessions
-const sess = {
-  secret: 'longrandompass3431',
-  cookie: {
-    expires: 10 * 60 * 1000,
-    secure: false,
-    httpOnly: true,
-    sameSite: "strict",
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({ db: sequelize }),
-};
-app.use(session(sess));
-
-// Sync Sequelize and drop/recreate tables 
-sequelize.sync({ force: true }).then(() => {
+// Synchronize Sequelize with the database
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("Server is now listening on port " + PORT);
   });
 });
